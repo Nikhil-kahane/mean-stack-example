7ca8816b332ae49258ea257d9229d989291307c2
Nikhil-kahane <92579780+Nikhil-kahane@users.noreply.github.com>
2025-05-22 18:27:51 +0530

    Update employee.service.ts
    edited private url = 'http://localhost:5200'; to 
    private url = '/api'; 
    
    This change ensures all API calls from the frontend go through the Nginx reverse proxy (which forwards /api to the backend), making your setup production-ready and avoiding direct localhost issues.

--- client/src/app/employee.service.ts
+++ client/src/app/employee.service.ts
@@ -6,7 +6,7 @@ import { Employee } from './employee';
   providedIn: 'root'
 })
 export class EmployeeService {
-  private url = 'http://localhost:5200';
+  private url = '/api';
   employees$ = signal<Employee[]>([]);
   employee$ = signal<Employee>({} as Employee);
   
